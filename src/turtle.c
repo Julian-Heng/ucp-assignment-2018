ef58fd7ada2bb3e7a48f4f06fc99adca773577a2
Julian Heng <19473701@student.curtin.edu.au>
2018-09-28 20:06:36 +0800

    Change turtle structure

--- src/turtle.c
+++ src/turtle.c
@@ -10,15 +10,18 @@
 #define FALSE 0
 #define TRUE !FALSE
 
+#define INVALID_ARGS 1
+#define FILE_NOT_FOUND 2
+
+#define MIN_ARGS 1
+
 int main(int argc, char** argv)
 {
-    int returnCode = 0;
-
+    int returnCode, rows, cols;
     char* filename;
     char** fileContents;
 
-    int rows, cols;
-
+    returnCode = 0;
     filename = NULL;
     fileContents = NULL;
     rows = 0;
@@ -26,20 +29,27 @@ int main(int argc, char** argv)
 
     if (checkArgs(argc, argv))
     {
-        if (processArgs(argc, argv, &filename) &&
+        if (processArgs(argc, argv, &filename, &returnCode) &&
             readFileToArray(filename, &fileContents, &rows, &cols))
         {
             processCommands(fileContents, rows);
         }
         else
         {
-            returnCode = 2;
+            if (returnCode)
+            {
+                returnCode = FILE_NOT_FOUND;
+            }
+            else
+            {
+                returnCode = FALSE;
+            }
         }
     }
     else
     {
         printUsage();
-        returnCode = 1;
+        returnCode = INVALID_ARGS;
     }
 
     freeArray((void***)&fileContents, rows);
@@ -50,100 +60,131 @@ int main(int argc, char** argv)
 
 int checkArgs(int argc, char** argv)
 {
-    int isValid = FALSE;
-    if (argc > 1)
+    int isValid;
+
+    isValid = FALSE;
+
+    if (argc > MIN_ARGS)
     {
         isValid = TRUE;
     }
+
     return isValid;
 }
 
-int processArgs(int argc, char** argv, char** filename)
+int processArgs(int argc, char** argv, char** filename, int* returnCode)
 {
-    int returnCode = 1;
-
     if (stringCompare(argv[1], "-h") ||
         stringCompare(argv[1], "--help"))
     {
         printUsage();
-        returnCode = 0;
+        *returnCode = FALSE;
     }
     else if (stringCompare(argv[1], "--version"))
     {
         printVersion();
-        returnCode = 0;
+        *returnCode = FALSE;
     }
     else
     {
         initStringWithContents(filename, argv[1]);
+        *returnCode = TRUE;
     }
 
-    return returnCode;
+    return *returnCode;
 }
 
 void processCommands(char** commandArr, int numCommands)
 {
-    int errLine = 0;
+    int errLine;
+
+    errLine= 0;
+
     if (prepareCommands(commandArr, numCommands, &errLine))
     {
-        printStringArray("%s\n", commandArr, numCommands);
+        /* Actual Drawing */
     }
     else
     {
-        fprintf(stderr, "%s %d\n", "Invalid command on line", errLine);
+        fprintf(stderr, "%s %d: ", "Invalid command on line", errLine);
+        fprintf(stderr, "%s\n", commandArr[errLine - 1]);
     }
 }
 
-int prepareCommands(char** commandArr, int numCommands, int* errLine)
+int prepareCommands(
+    char** commandArr,
+    int numCommands,
+    int* errLine)
 {
-    int commandValid = FALSE;
-    int i;
+    int commandValid, i;
 
     double angle;
     double length;
     int color;
     char pat;
-    char buff[256];
 
+    char tempStr[256];
+    char* origStr;
+
+    commandValid = FALSE;
+    origStr = NULL;
     i = 0;
 
     do
     {
         commandValid = FALSE;
         resetVariables(&angle, &length, &color, &pat);
-        upper(commandArr[i]);
 
-        if (strstr(commandArr[i], "ROTATE") &&
-            sscanf(commandArr[i], "%s %lf", buff, &angle) == 2 &&
-            doubleBoundaryCheck(angle, -360.0, 360.0))
+        initStringWithContents(&origStr, commandArr[i]);
+
+        if (countWhiteSpace(commandArr[i]) == 1)
         {
-            commandValid = TRUE;
+            upper(commandArr[i]);
+            sscanf(commandArr[i], "%s", tempStr);
+            if ((stringCompare(tempStr, "ROTATE")) &&
+                sscanf(commandArr[i], "%s %lf", tempStr, &angle) == 2 &&
+                doubleBoundaryCheck(angle, -360.0, 360.0))
+            {
+                commandValid = TRUE;
+            }
+            else if ((stringCompare(tempStr, "MOVE") ||
+                     (stringCompare(tempStr, "DRAW"))) &&
+                     sscanf(commandArr[i], "%s %lf", tempStr, &length) == 2 &&
+                     doubleBoundaryCheck(length, 0.0, -1.0))
+            {
+                commandValid = TRUE;
+            }
+            else if (stringCompare(tempStr, "FG") &&
+                     sscanf(commandArr[i], "%s %d", tempStr, &color) == 2 &&
+                     integerBoundaryCheck(color, 0, 15))
+            {
+                commandValid = TRUE;
+            }
+            else if (stringCompare(tempStr, "BG") &&
+                     sscanf(commandArr[i], "%s %d", tempStr, &color) == 2 &&
+                     integerBoundaryCheck(color, 0, 7))
+            {
+                commandValid = TRUE;
+            }
+            else if (stringCompare(tempStr, "PATTERN") &&
+                     sscanf(commandArr[i], "%s %c", tempStr, &pat) == 2 &&
+                     pat != ' ')
+            {
+                commandValid = TRUE;
+            }
+            else
+            {
+                freePtr((void**)&commandArr[i]);
+                initStringWithContents(&commandArr[i], origStr);
+            }
         }
-        else if ((strstr(commandArr[i], "MOVE") ||
-                 (strstr(commandArr[i], "DRAW"))) &&
-                 sscanf(commandArr[i], "%s %lf", buff, &length) == 2 &&
-                 doubleBoundaryCheck(length, 0.0, -1.0))
+        else
         {
-            commandValid = TRUE;
+            freePtr((void**)&commandArr[i]);
+            initStringWithContents(&commandArr[i], origStr);
         }
-        else if (strstr(commandArr[i], "FG") &&
-                 sscanf(commandArr[i], "%s %d", buff, &color) == 2 &&
-                 integerBoundaryCheck(color, 0, 15))
-        {
-            commandValid = TRUE;
-        }
-        else if (strstr(commandArr[i], "BG") &&
-                 sscanf(commandArr[i], "%s %d", buff, &color) == 2 &&
-                 integerBoundaryCheck(color, 0, 7))
-        {
-            commandValid = TRUE;
-        }
-        else if (strstr(commandArr[i], "PATTERN") &&
-                 sscanf(commandArr[i], "%s %c", buff, &pat) == 2 &&
-                 pat != ' ')
-        {
-            commandValid = TRUE;
-        }
+
+        freePtr((void**)&origStr);
     } while (++i < numCommands && commandValid);
 
     *errLine = i;
@@ -167,16 +208,16 @@ void printUsage(void)
         "\n",
         "Valid commands:\n",
         "\n",
-        "    +---------+-------+--------------------+\n",
-        "    | Command | Type  | Range              |\n",
-        "    +---------+-------+--------------------+\n",
-        "    | ROTATE  | float | 0 to 360 inclusive |\n",
-        "    | MOVE    | float | Positive           |\n",
-        "    | DRAW    | float | Positive           |\n",
-        "    | FG      | int   | 0 to 15 inclusive  |\n",
-        "    | BG      | int   | 0 to 7 inclusive   |\n",
-        "    | PATTERN | char  | Any character      |\n",
-        "    +---------+-------+--------------------+\n",
+        "    +---------+-------+-----------------------+\n",
+        "    | Command | Type  | Range                 |\n",
+        "    +---------+-------+-----------------------+\n",
+        "    | ROTATE  | float | -360 to 360 inclusive |\n",
+        "    | MOVE    | float | Positive              |\n",
+        "    | DRAW    | float | Positive              |\n",
+        "    | FG      | int   | 0 to 15 inclusive     |\n",
+        "    | BG      | int   | 0 to 7 inclusive      |\n",
+        "    | PATTERN | char  | Any character         |\n",
+        "    +---------+-------+-----------------------+\n",
         "\n",
         "EOF"
     };
--- src/turtle.h
+++ src/turtle.h
@@ -1,7 +1,7 @@
 #ifndef TURTLE_H
 #define TURTLE_H
 int checkArgs(int argc, char** argv);
-int processArgs(int argc, char** argv, char** filename);
+int processArgs(int argc, char** argv, char** filename, int* returnCode);
 void processCommands(char** commandArr, int numCommands);
 int prepareCommands(char** commandArr, int numCommands, int* errLine);
 void resetVariables(double* angle, double* length, int* color, char* pat);
