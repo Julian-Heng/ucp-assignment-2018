81aaae2cd13a0a5850a946eb37e738ae83ec9009
Julian Heng <19473701@student.curtin.edu.au>
2018-10-02 01:59:36 +0800

    Add linked list

---
+++ src/linkedList.c
@@ -0,0 +1,219 @@
+#include <stdlib.h>
+
+#include "linkedList.h"
+
+static void setNextToNode(LinkedListNode* node, LinkedListNode* nextNode);
+static void setPrevToNode(LinkedListNode* node, LinkedListNode* prevNode);
+static void setNextInList(LinkedListNode* node);
+static void setPrevInList(LinkedListNode* node);
+
+LinkedListNode* initNode(void* newValue)
+{
+    LinkedListNode* newNode;
+    newNode = (LinkedListNode*)malloc(sizeof(LinkedListNode));
+
+    newNode -> next = NULL;
+    newNode -> prev = NULL;
+    newNode -> value = newValue;
+
+    return newNode;
+}
+
+LinkedList* initList()
+{
+    LinkedList* newList;
+
+    newList = (LinkedList*)malloc(sizeof(LinkedList));
+
+    newList -> head = NULL;
+    newList -> tail = NULL;
+
+    return newList;
+}
+
+void insertFirst(LinkedList* list, void* newValue)
+{
+    LinkedListNode* newNode;
+
+    newNode = initNode(newValue);
+
+    if (isEmpty(list))
+    {
+        list -> head = newNode;
+        list -> tail = newNode;
+    }
+    else
+    {
+        setPrevToNode(list -> head, newNode);
+        setNextToNode(newNode, list -> head);
+        list -> head = newNode;
+    }
+}
+
+void insertLast(LinkedList* list, void* newValue)
+{
+    LinkedListNode* newNode;
+
+    newNode = initNode(newValue);
+
+    if (isEmpty(list))
+    {
+        list -> head = newNode;
+        list -> tail = newNode;
+    }
+    else
+    {
+        setPrevToNode(newNode, list -> tail);
+        setNextToNode(list -> tail, newNode);
+        list -> tail = newNode;
+    }
+}
+
+LinkedListNode* removeFirst(LinkedList* list)
+{
+    LinkedListNode* remove;
+
+    remove = NULL;
+
+    if (! isEmpty(list))
+    {
+        remove = list -> head;
+        if (list -> head == list -> tail)
+        {
+            list -> head = NULL;
+            list -> tail = NULL;
+        }
+        else
+        {
+            setNextInList(list -> head);
+        }
+    }
+
+    return remove;
+}
+
+LinkedListNode* removeLast(LinkedList* list)
+{
+    LinkedListNode* remove;
+
+    remove = NULL;
+
+    if (! isEmpty(list))
+    {
+        remove = list -> tail;
+        if (list -> head == list -> tail)
+        {
+            list -> head = NULL;
+            list -> tail = NULL;
+        }
+        else
+        {
+            setPrevInList(list -> tail);
+        }
+    }
+
+    return remove;
+}
+
+void* peekFirst(LinkedList* list)
+{
+    void* nodeValue;
+
+    nodeValue = NULL;
+
+    if (! isEmpty(list))
+    {
+        nodeValue = list -> head -> value;
+    }
+
+    return nodeValue;
+}
+
+void* peekLast(LinkedList* list)
+{
+    void* nodeValue;
+
+    nodeValue = NULL;
+
+    if (! isEmpty(list))
+    {
+        nodeValue = list -> tail -> value;
+    }
+
+    return nodeValue;
+}
+
+void clearListStack(LinkedList** list)
+{
+    LinkedListNode* inList;
+
+    inList = NULL;
+
+    if (! isEmpty(*list))
+    {
+        while ((*list) -> head != NULL)
+        {
+            inList = (*list) -> head;
+            (*list) -> head = (*list) -> head -> next;
+            free(inList);
+            inList = NULL;
+        }
+    }
+
+    if (*list)
+    {
+        free(*list);
+        *list = NULL;
+    }
+}
+
+void clearListMalloc(LinkedList** list)
+{
+    LinkedListNode* inList;
+
+    inList = NULL;
+
+    if (! isEmpty(*list))
+    {
+        while ((*list) -> head)
+        {
+            inList = (*list) -> head;
+            (*list) -> head = (*list) -> head -> next;
+            free(inList -> value);
+            inList -> value = NULL;
+            free(inList);
+            inList = NULL;
+        }
+    }
+
+    if (*list)
+    {
+        free(*list);
+        *list = NULL;
+    }
+}
+
+void setNextToNode(LinkedListNode* node, LinkedListNode* nextNode)
+{
+    node -> next = nextNode;
+}
+
+void setPrevToNode(LinkedListNode* node, LinkedListNode* prevNode)
+{
+    node -> prev = prevNode;
+}
+
+void setNextInList(LinkedListNode* node)
+{
+    node = node -> next;
+}
+
+void setPrevInList(LinkedListNode* node)
+{
+    node = node -> prev;
+}
+
+int isEmpty(LinkedList* list)
+{
+    return (list && list -> head == NULL && list -> tail == NULL);
+}
---
+++ src/linkedList.h
@@ -0,0 +1,32 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct LinkedListNode LinkedListNode;
+typedef struct LinkedList LinkedList;
+
+struct LinkedListNode
+{
+    struct LinkedListNode* next;
+    struct LinkedListNode* prev;
+    void* value;
+};
+
+struct LinkedList
+{
+    LinkedListNode* head;
+    LinkedListNode* tail;
+};
+
+LinkedListNode* initNode(void* newValue);
+LinkedList* initList();
+void insertFirst(LinkedList* list, void* newValue);
+void insertLast(LinkedList* list, void* newValue);
+LinkedListNode* removeFirst(LinkedList* list);
+LinkedListNode* removeLast(LinkedList* list);
+void* peekFirst(LinkedList* list);
+void* peekLast(LinkedList* list);
+void clearListStack(LinkedList** list);
+void clearListMalloc(LinkedList** list);
+int isEmpty(LinkedList* list);
+
+#endif
