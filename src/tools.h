1a6ff5d57c674cf246396e6d00a9ea83b1873fb5
Julian Heng <19473701@student.curtin.edu.au>
2018-10-07 21:01:22 +0800

    Add commenting to header files

--- src/tools.h
+++ src/tools.h
@@ -1,21 +1,32 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+/* Constants */
 #define TOLERANCE 0.0000001
 #define PI 3.14159265358979323846
 
+/* Macros */
+
+/* Checks if one double is more than or equal to another double */
 #define DOUBLE_CHECK(a, b) \
     (((a) > (b)) || (doubleCompare((a), (b))))
 
+/* Check if an integer is within the bounds of two integers, inclusive */
 #define INT_BOUND(a, low, up) \
     (((a) >= (low)) && ((up) >= (a)))
+
+/* Check if a double is within the bounds of two doubles, inclusive */
 #define DOUBLE_BOUND(a, low, up) \
     ((DOUBLE_CHECK((a), (low))) && (DOUBLE_CHECK((up), (a))))
 
+/* Convert degrees to radians */
 #define DEG_TO_RAD(i) ((i) * (PI / 180.0))
-#define GET_STACK_2D_ARRAY_SIZE(arr) \
+
+/* Get the size of a stack array */
+#define GET_STACK_ARRAY_SIZE(arr) \
     (sizeof((arr)) / sizeof((arr)[0]))
 
+/* Function prototypes */
 void initString(char** str, int len);
 void initStringWithContents(char** str, char* contents);
 void initStringArray(char***, int rows, int cols);
